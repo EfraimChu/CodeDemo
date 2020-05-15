8106d437c5b1f7cdba2576d3b03984c503f1e2f0
efraim.zhu <lovexiyou06@gmail.com>
2020-05-15 11:02:34 +0800

    leetcode. 35

--- 35.搜索插入位置.java
+++ 35.搜索插入位置.java
@@ -3,35 +3,68 @@
  * @lc app=leetcode.cn id=35 lang=java
  *
  * [35] 搜索插入位置
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+你可以假设数组中无重复元素。
+
+示例 1:
+
+输入: [1,3,5,6], 5
+输出: 2
+示例 2:
+
+输入: [1,3,5,6], 2
+输出: 1
+示例 3:
+
+输入: [1,3,5,6], 7
+输出: 4
+示例 4:
+
+输入: [1,3,5,6], 0
+输出: 0
  */
 
-//todo 还没通过
 // @lc code=start
-class Solution {
-    public int searchInsert(int[] nums, int target) {
+class Solution35 {
+    public static void main(String[] args) {
         int[] test = { 1, 3, 5, 6 };
-    
-        return searchArray(test, 0, test.length - 1, target);
+          System.out.println(new Solution35().searchInsert(test, 2));
+    }
+
+    /**
+     * 思路：
+     * 找到第一个>=target 值得坐标即可；
+     * 如果nums[0]>target return 0 ;
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        return searchArray(nums, 0, nums.length - 1, target);
     }
 
     public int searchArray(int[] nums, int begin, int end, int findInt) {
-        if (begin >= end) {
-            if(nums[end] == findInt){
-                return end;
-            } else if((end-1)>=0 ){
-                return end-1;
+        if (findInt <= nums[0]) {
+            return 0;
+        }
+        while (begin <= end) {
+            int mid = (end + begin) / 2;
+            if (nums[mid] <= findInt) {
+                if (nums[mid] == findInt) {
+                    return mid;
+                }
+                begin= mid+1;
+            }else{
+                end = mid-1;
             }
-            return end+1;
         }
-        int mid = (end + begin) / 2;
-        if (nums[mid] < findInt) {
-            return searchArray(nums, mid + 1, end, findInt);
-        }
-        if (nums[mid] == findInt) {
-            return mid;
-        }
-        return searchArray(nums, begin, end - 1, findInt);
 
+        return end+1;
     }
 
 }
